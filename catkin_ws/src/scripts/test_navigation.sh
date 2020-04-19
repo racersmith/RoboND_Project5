3a7d3b75bc80e73d8215eb0c7bd034b9cd9302f2
Josh Smith <racersmith@gmail.com>
2020-04-19 23:53:35 +0000

    add navigation test script

---
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Change to our simple demo map
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/RoboND_Project5/catkin_ws/src/world/basic.world
+
+# Launch world
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+
+sleep 5
+
+# Launch AMCL
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+
+sleep 5
+
+# Launch rviz
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" 
