29810ecfdf91fd579088400c9b64dce56f70e6e9
Josh Smith <racersmith@gmail.com>
2020-04-23 02:28:26 +0000

    instatiate only one action client

--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -5,11 +5,25 @@
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-double goal_1[3] = {2.75, -0.83, -1.35};
-double goal_2[3] = {1.0, -4.5, -2.9};
 
-  
-bool moveTo(double x, double y, double w){
+double x[2] = {2.75, 1.0};
+double y[2] = {-0.83, -4.6};
+double w[2] = {-1.35, -2.9};
+
+
+void simulatePickup(){
+  // Wait 5 sec to simulate object pickup
+  ROS_INFO("Simulating object pickup");
+  ROS_INFO("Picking up object... ");
+  ros::Duration(5.0).sleep();
+  ROS_INFO("Pickup complete");
+}
+
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
@@ -25,61 +39,40 @@ bool moveTo(double x, double y, double w){
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = x;
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = w;
+  goal.target_pose.pose.position.x = x[0];
+  goal.target_pose.pose.position.y = y[0];
+  goal.target_pose.pose.orientation.w = w[0];
 
   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal pose");
+  ROS_INFO("Sending pickup location");
   ac.sendGoal(goal);
-
-  // Wait an infinite time for the results
   ac.waitForResult();
-  
-  // Check if the robot reached its goal
+
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-    ROS_INFO("Hooray, goal reached");
-  	return true;
-  }
-  else {
-    ROS_INFO("Booooo, goal was not reached");
-  	return false;
-  }
-}
-
-void simulatePickup(){
-  // Wait 5 sec to simulate object pickup
-  ROS_INFO("Simulating object pickup");
-  std::cout << "Picking up object... ";
-  double end_time = ros::Time::now().toSec() + 5.0;
-  while (end_time > ros::Time::now().toSec()){
-//     ros::spinOnce();
-  }
-  std::cout << "Complete" << std::endl;
-}
-
-
-int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "pick_objects");
-
-  bool status;
+    ROS_INFO("Pickup location reached");
+    
+    simulatePickup();
   
-  
-  status = moveTo(goal_1[0], goal_1[1], goal_1[2]);
-  ROS_INFO("JUST EXITED FIRST GOAL");
-  std::cout << "Exit move 1, " << status << std::endl;
-  if (status){
-    std::cout << "Success" << std::endl;
-	simulatePickup();
-	status = moveTo(goal_2[0], goal_2[1], goal_2[2]);
+    // Define a position and orientation for the robot to reach
+    goal.target_pose.pose.position.x = x[1];
+    goal.target_pose.pose.position.y = y[1];
+    goal.target_pose.pose.orientation.w = w[1];
+
+    // Send the goal position and orientation for the robot to reach
+    ROS_INFO("Sending dropoff location");
+    ac.sendGoal(goal);
+    ac.waitForResult();
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO("Drop-off location reached");
+    else
+      ROS_INFO("Failed to reach Drop-off location");
   }
-  status = moveTo(goal_2[0], goal_2[1], goal_2[2]);
+  else{
+    ROS_INFO("Failed to reach pickup location");
+  }
   
-  if (status)
-  	ROS_INFO("Drop off location reached");
-  else
-    ROS_INFO("Failed to reach drop off location");
+
   
   ros::spin();
   return 0;
