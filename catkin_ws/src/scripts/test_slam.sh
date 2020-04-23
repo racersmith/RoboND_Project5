0e8a6cf37560b784a3f2fd7a53abc97a29f58f6c
Josh Smith <racersmith@gmail.com>
2020-04-23 04:11:10 +0000

    working on fixing slam issues

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -6,7 +6,10 @@ xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; ro
 
 sleep 5
 
-xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" & 
+# xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" & 
+
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch gmapping slam_gmapping.launch" & 
+
 
 sleep 5
 
