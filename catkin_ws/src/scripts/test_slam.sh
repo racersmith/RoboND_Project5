0fd11a04f4894f021882ccaf0ac7558a071695e0
Josh Smith <racersmith@gmail.com>
2020-04-24 00:02:35 +0000

    add map environment var

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -7,7 +7,6 @@ xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; ro
 sleep 5
 
 # xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" & 
-
 xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch gmapping slam_gmapping.launch" & 
 
 
