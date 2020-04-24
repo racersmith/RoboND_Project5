2e2c01167ca357247ae0c18398c4fe47bf06d812
Josh Smith <racersmith@gmail.com>
2020-04-24 23:55:22 +0000

    adding notes and pull

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+# Update workspace
 apt-get update && apt-get upgrade -y
+
+# Install missing package
+pip install rospkg
+
+# Configure git
 git config --global user.name "Josh Smith"
 git config --global user.email "racersmith@gmail.com"
 git config --global credential.helper 'cache --timeout=9999'
-pip install rospkg
+
+# Grab latest
+git pull origin master
+
+# Make the package
 cd catkin_ws
 catkin_make
