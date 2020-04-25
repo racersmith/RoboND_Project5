f26c84182b7434adfb0e3f4eb2644d4a889ef8b3
Josh Smith <racersmith@gmail.com>
2020-04-25 00:01:07 +0000

    adding home service script

---
+++ catkin_ws/src/scripts/home_service.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Change to our simple demo map
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/RoboND_Project5/catkin_ws/src/world/basic.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/RoboND_Project5/catkin_ws/src/map/basic.yaml
+
+# Launch world
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+sleep 5
+
+# Launch AMCL
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+
+# Launch rviz
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun rviz rviz -d /home/workspace/RoboND_Project5/catkin_ws/src/rvizConfig/full.rviz" &
+sleep 5
+
+# Launch virtual object
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun add_markers virtual_objects" & 
+sleep 5
+
+# Launch pick
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects"
