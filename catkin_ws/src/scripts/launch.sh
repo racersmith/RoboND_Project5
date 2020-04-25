57c658e5abef018c3561a2021184e1a9511c08cf
Josh Smith <racersmith@gmail.com>
2020-04-24 21:41:18 -0600

    Change world in launch file

--- catkin_ws/src/scripts/launch.sh
+++ catkin_ws/src/scripts/launch.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-xterm -e " gazebo" &
+
+# Change to our simple demo map
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/RoboND_Project5/catkin_ws/src/world/basic.world
+
+# Launch Gazebo Simulation
+xterm -e " source /home/workspace/RoboND_Project5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
 sleep 5
+
+# Launch roscore
 xterm -e " source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
+
+# Launch rviz
 xterm -e " rosrun rviz rviz"
